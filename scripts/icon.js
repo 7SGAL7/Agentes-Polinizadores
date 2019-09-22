51ff70c6935f17132d7114a7b777ab2b62e4186d
sgal <samiraes521@gmail.com>
2019-09-22 10:11:39 -0500

    ultimmo

---
+++ scripts/icon.js
@@ -0,0 +1,45 @@
+let link = document.getElementById("enlace");
+let abrir = document.getElementById("open");
+let close = false;
+let botones = document.getElementsByClassName("menu-item");
+let icono  = document.getElementById("icon");
+
+function apertura(){
+    if(close){
+        link.style.width = '90vw';
+        close = false;
+        icono.style.opacity = '0';
+    }else{
+        link.style.width = '0';
+        link.style.overflow = 'hidden';
+        close = true;
+        icono.style.opacity = '100';
+    }
+}
+
+
+window.addEventListener('click', function(e){
+    console.log(e.target);
+    if(close === false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            link.style.width = '0%';
+            link.style.overflow = 'hidden';
+            close = true;
+            icono.style.opacity = '100';
+        }
+    }
+});
+
+
+icono.addEventListener('click', function(){
+    apertura();
+});
+
+
+
+window.addEventListener('load', function(){
+    if(this.screen.width <= 840){
+        apertura();
+    }
+});
---
+++ scripts/main.js
@@ -0,0 +1,68 @@
+let icono  = document.getElementById("icon");
+let link = document.getElementById("enlace");
+let abrir = document.getElementById("open");
+let close = false;
+let botones = document.getElementsByClassName("menu-item");
+
+function Icon(){
+    let positon = window.pageYOffset;
+    if(positon > 560 && positon < 1080){   
+        icono.style.background = "rgba(250, 169, 6,.8)";
+        icono.style.transition = '1s'
+    }else if(positon > 1079 && positon < 2280){
+        icono.style.background = "rgba(112,60,0,.8)";
+        icono.style.transition = '1s';
+    }if(positon <= 560 || positon > 2280){
+        icono.style.background = "rgba(88, 214, 141,.6)";
+        icono.style.transition = '1s';
+    }
+}
+
+function apertura(){
+    if(close){
+        link.style.width = '90vw';
+        close = false;
+        icono.style.opacity = '0';
+    }else{
+        link.style.width = '0';
+        link.style.overflow = 'hidden';
+        close = true;
+        icono.style.opacity = '100';
+    }
+}
+
+
+window.addEventListener('click', function(e){
+    console.log(e.target);
+    if(close === false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            link.style.width = '0%';
+            link.style.overflow = 'hidden';
+            close = true;
+            icono.style.opacity = '100';
+        }
+    }
+});
+
+
+icono.addEventListener('click', function(){
+    apertura();
+});
+
+
+window.addEventListener('scroll',function(){
+  Icon();
+});
+
+window.addEventListener('load', function(){
+    if(this.screen.width <= 840){
+        apertura();
+    }
+    Icon();
+});
+
+window.addEventListener('resize', function(){
+    Icono();
+});
+
